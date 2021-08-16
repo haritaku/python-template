c90c43202860018892661d4eb116e0ce041a2316
haritaku <hari1147taku@gmail.com>
2021-08-16 21:57:01 +0900

    logの設定追加

--- src/utils/common.py
+++ src/utils/common.py
@@ -7,7 +7,12 @@ from src import LOGS_PATH, ROOT_PATH
 from .io import load_yaml
 
 
-def set_log_cfg():
+def set_log():
+    """loggingの設定
+
+    リポジトリのルートに存在するlogging_cfg.ymlからloggingの設定を読み込み反映する。\n
+    logファイルはrepository_root/logsに保存され、(呼び出し元のファイル名).logというファイル名で保存される。
+    """
     fpath_log_cfg = ROOT_PATH / "logging_cfg.yml"
     caller_stem = Path(inspect.stack()[1].filename).stem
     log_cfgs = load_yaml(fpath_log_cfg)
