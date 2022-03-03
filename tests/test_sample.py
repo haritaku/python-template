67f844cbaf014f90ee019b25bff5e2292a5f0100
haritaku <hari1147taku@gmail.com>
2022-03-04 00:42:23 +0900

    add pytest workflow

---
+++ tests/test_sample.py
@@ -0,0 +1,10 @@
+def is_even(x: int) -> bool:
+    if x % 2 == 0:
+        return True
+    else:
+        return False
+
+
+def test_is_even():
+    assert is_even(1) is False
+    assert is_even(2) is True
